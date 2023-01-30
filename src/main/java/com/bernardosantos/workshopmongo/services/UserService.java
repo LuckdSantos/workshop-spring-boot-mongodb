58322aa85470e4ab0103bd4fcd0886b3659e804d
Bernardo Santos <belucksan@gmail.com>
2023-01-30 16:25:58 -0300

    Obtendo um usuario por id

--- src/main/java/com/bernardosantos/workshopmongo/services/UserService.java
+++ src/main/java/com/bernardosantos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.bernardosantos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bernardosantos.workshopmongo.domain.User;
 import com.bernardosantos.workshopmongo.repository.UserRepository;
+import com.bernardosantos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,11 @@ public class UserService {
 		
 		return repo.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
